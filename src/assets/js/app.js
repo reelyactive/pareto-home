f50520bc5b88b5473f9622ef12a9df136a8a5d94
Félix-Antoine Brunet <felix.a.brunet@gmail.com>
2016-04-25 00:51:34 -0400

    Hi-res homepage image + more mobile adjustments

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
   $('.animated').viewportChecker({
     classToAdd: 'fadeInUp',
     classToRemove: 'animated',
-    offset: 100, // Class to add to the elements when they are visible
+    offset: 50, // Class to add to the elements when they are visible
   });
 });
