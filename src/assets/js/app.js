9897b04e617c2b3d3cc0c27ebe5a53f6e3121c37
Félix-Antoine Brunet <felix.a.brunet@gmail.com>
2016-04-26 13:20:08 -0400

    CTA buttons mods + text drop shadow

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
+  // Animate element when in view
   $('.animated').viewportChecker({
     classToAdd: 'fadeInUp',
     classToRemove: 'animated',
     offset: 50, // Class to add to the elements when they are visible
   });
 
+  // B-Lazy load plugin for loading images
   var bLazy = new Blazy();
+
+  // Smooth scroll for anchor links
+  $('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 700, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
 });
